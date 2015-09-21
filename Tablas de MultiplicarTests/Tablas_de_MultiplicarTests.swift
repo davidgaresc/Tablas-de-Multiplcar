ad16b12229144fd60f24df47530f0d73177dfb08
David Antonio Gracia Escobar <davidgaresc87@hotmail.com>
2015-09-20 19:23:08 -0500

    Initial Commit

---
+++ Tablas de MultiplicarTests/Tablas_de_MultiplicarTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tablas_de_MultiplicarTests.swift
+//  Tablas de MultiplicarTests
+//
+//  Created by cCeo on 20/09/15.
+//  Copyright (c) 2015 cCeo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Tablas_de_MultiplicarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
